76d28d7c04d375df526550272bbd0e1d201880a5
vidura-12 <viduranirmal195@gmail.com>
2025-04-23 18:49:51 +0530

    Menu Route Bug fix

--- RestaurantManagementService/Data/Models/MenuDto.cs
+++ RestaurantManagementService/Data/Models/MenuDto.cs
@@ -7,6 +7,6 @@
         public string MenuName { get; set; }
         public string Description { get; set; }
         public bool IsActive { get; set; }
-        public bool IsApproved { get; set; }
+        public bool menuApproved { get; set; }
     }
 }
