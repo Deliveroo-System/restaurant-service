e0cd8f79e33366ae2e7499c203e5e79a813709fb
vidura-12 <viduranirmal195@gmail.com>
2026-02-28 18:36:00 +0530

    Implement Menu Items Routs

---
+++ RestaurantManagementService/Services/IMenuItemServicecs.cs
@@ -0,0 +1,22 @@
+﻿using RestaurantManagementService.Data.Models;
+
+namespace RestaurantManagementService.Services
+{
+    public interface IMenuItemService
+    {
+        Task<IEnumerable<MenuItemDto>> GetMenuItemsAsync(int restaurantId, int menuId);
+        Task<MenuItemDto> GetMenuItemByIdAsync(int restaurantId, int menuId, int menuItemId);
+
+        Task<int> ManageMenuItemAsync(
+     string action,
+     int? menuItemId,
+     int menuId,
+     string name,
+     string description,
+     decimal? price,
+     bool? isAvailable,
+     string imageUrl); // This argument is required
+
+    }
+
+}
